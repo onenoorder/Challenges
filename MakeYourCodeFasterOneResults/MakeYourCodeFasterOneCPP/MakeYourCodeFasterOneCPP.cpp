88ce4e32bdae4649992929ca8d0a524ac78f028f
Gerhard <gerhard.kroes@alten.nl>
2025-03-25 14:40:50 +0100

    Update MakeYourCodeFasterOneCPP.cpp

--- MakeYourCodeFasterOneResults/MakeYourCodeFasterOneCPP/MakeYourCodeFasterOneCPP.cpp
+++ MakeYourCodeFasterOneResults/MakeYourCodeFasterOneCPP/MakeYourCodeFasterOneCPP.cpp
@@ -13,14 +13,13 @@ void runChallengeWith(int cycles, long long expectedResult) {
 
     auto finish = high_resolution_clock::now();
 
-    std::cout << (result == expectedResult ? "Great Success!" : "Not quite there yet, keep going!") << "\n\n";
-
     auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(finish - start);
-    std::cout << microseconds.count() << "µs\n";
-    std::cout << microseconds.count() / 1000 << "ms\n";
-    std::cout << microseconds.count() / 1000000 << "s\n";
 
-    std::cout << "\n" << (microseconds.count() / 1000000 < 1 ? "Another satisfied customer!" : "The customer couldn't wait that long and walked away....") << "\n\n";
+    std::cout << "Your solution was: " << result << "\n";
+    std::cout << (result == expectedResult ? "Great Success!" : "Not quite there yet, keep going!") << "\n";
+    std::cout << "Your solution ran in: " << microseconds.count() / 1000 << "ms\n";
+    std::cout << "That is about: " << microseconds.count() / 1000000 << "s\n";
+    std::cout << (microseconds.count() / 1000000 < 1 ? "Another satisfied customer!" : "The customer couldn't wait that long and walked away....") << "\n\n";
 }
 
 int main()
